85b07e37e807aeaf8e93c119a21eb3f00aedf01e
Sanjaya <sanjaya.maharana@iail.com>
2023-03-17 02:28:40 +0530

    changes

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate, migrate
-from flask_bcrypt import Bcrypt
 
 
 
