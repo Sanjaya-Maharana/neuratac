ffed7158ecd2bb778b68d13fc2af6df3347d4a42
ACER <SanjayaMaharana145@gmail.com>
2023-07-26 17:21:46 +0530

    changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect
-
+import os
 
 
 app = Flask(__name__)
@@ -15,4 +15,7 @@ def contact_us():
     return render_template('contact_us.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use the environment variable for the port if available, or fallback to 8000
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host='0.0.0.0', port=port)
+
