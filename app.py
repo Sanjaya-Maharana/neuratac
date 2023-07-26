5320dbec6e61df341d245d62acddae0bab9f65d5
ACER <SanjayaMaharana145@gmail.com>
2023-07-26 17:05:18 +0530

    changes

--- app.py
+++ app.py
@@ -9,16 +9,10 @@ app.debug = True
 @app.route("/")
 def home():
     return render_template('home.html')
-@app.route("/resume")
-def resume():
-    return render_template('resume.html')
-@app.route("/freelence")
-def freelence():
-    return render_template('freelence.html')
 
 @app.route("/contact_us")
 def contact_us():
     return render_template('contact_us.html')
 
 if __name__ == '__main__':
-    app.run(port=8000)
+    app.run(debug=True)
