e1f67713646c8555e33a9a636fd3f1b15ef487bc
Sanjaya Maharana <SanjayaMaharana145@gmail.com>
2023-08-27 03:58:28 +0530

    changes

--- app.py
+++ app.py
@@ -24,6 +24,26 @@ def gallery():
 def contact():
     return render_template('contact.html')
 
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    email = request.form.get('email')  # Get the email from the POST request
+    # Here, you can implement the logic to handle the subscription.
+    # This could involve saving the email to a database, sending confirmation emails, etc.
+    return f"Subscription successful! Received email: {email}"
+
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    # Here, you can implement the logic to handle the message.
+    # This could involve sending emails, storing messages in a database, etc.
+
+    return "Message sent successfully!"
+
 if __name__ == '__main__':
     # Use the environment variable for the port if available, or fallback to 8000
     port = int(os.environ.get("PORT", 8000))
