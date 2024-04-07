843b9b4942d0b138374943a716fde9776124c07f
Sanjaya Maharana <SanjayaMaharana145@gmail.com>
2024-04-07 06:47:01 +0530

    update

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from datetime import datetime
 import sqlite3
 
 app = Flask(__name__)
-app.config['STATIC_FOLDER'] = 'static'
 
 # Configure your email settings
 smtp_server = 'smtp.gmail.com'
@@ -185,5 +184,4 @@ def send_message():
 
 if __name__ == '__main__':
     create_database()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0')
